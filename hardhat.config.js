cf5a4a9694f689df62dfdeab32503bd3a52a19fa
hrndzjuanc <juan.hernandez@bts.com>
2021-11-27 23:11:54 +0100

    SpainPunks

---
+++ hardhat.config.js
@@ -0,0 +1,29 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: 'https://rinkeby.infura.io/v3/a8c271ad1f954d9c9da408577d0c844a',
+      accounts: [
+        '0x059e3Fc9A87EF07557e0b1F5E3f37846165512E6'
+      ]
+    }
+  }
+};
